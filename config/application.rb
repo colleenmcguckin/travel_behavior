14b6f16c15684e748a78ff5a7dc2577a77bdfd5f
colleenmcguckin <colleenmcguckin@gmail.com>
2016-12-04 02:39:10 -0800

    setup for env variables

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,23 @@ Bundler.require(*Rails.groups)
 
 module TravelBehavior
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Try loading a YAML file at `./config/env.[environment].yml`, if it exists
+    # Kudos to Thomas Fuchs (http://mir.aculo.us) for the initial implementation
+    def load_env_file(environment = nil)
+      path = Rails.root.join("config", "env#{environment.nil? ? '' : '.'+environment}.yml")
+      return unless File.exist? path
+      config = YAML.load(ERB.new(File.new(path).read).result)
+      config.each { |key, value| ENV[key.to_s] = value.to_s }
+    end
+
+    # Load environment variables. config/env.yml contains defaults which are
+    # suitable for development. (This file is optional).
+    load_env_file
+
+    # Now look for custom environment variables, stored in env.[environment].yml
+    # For development, this file is not checked into source control, so feel
+    # free to tweak for your local development setup. Any values defined here
+    # overwrite the defaults in `env.yml`
+    load_env_file(Rails.env)
   end
 end
