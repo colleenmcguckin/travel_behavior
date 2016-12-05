19041c533a3ae66b7fbd0727ae9340c5ee051f24
colleenmcguckin <colleenmcguckin@gmail.com>
2016-12-04 18:42:58 -0800

    model test

--- app/models/document.rb
+++ app/models/document.rb
@@ -6,5 +6,6 @@ class Document < ActiveRecord::Base
   validates :title, presence: {message: 'Title the image.'}, on: :create
   validates :category, presence: {message: 'Pick the category.'}, on: :create
   validates :publication_date, presence: {message: 'Pick the publication date, or today.'}, on: :create
+  validates :content, presence: {message: 'Content could not be saved.'}, on: :create
 
 end
