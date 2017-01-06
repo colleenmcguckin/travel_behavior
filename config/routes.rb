8a61a429c58c7c2659a019c72c63667ec9067ac6
colleenmcguckin <colleenmcguckin@gmail.com>
2017-01-06 12:42:16 -0800

    root to home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :admins, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: 'pages#index'
+  root to: 'pages#home'
   resources :pages
   resources :images, only: [:index]
   resources :documents, only: [:index, :show]
