29dd3e141f0840086b3d85f3a8e8353c21307551
colleenmcguckin <colleenmcguckin@gmail.com>
2017-01-06 14:04:40 -0800

    try to fix IE

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,17 @@
 //= require mentions/mentions
 //= require pages
 
+
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement('style')
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      '@-ms-viewport{width:auto!important}'
+    )
+  )
+  document.querySelector('head').appendChild(msViewportStyle)
+}
+
 $(function() {
     function reposition() {
         var modal = $(this),
