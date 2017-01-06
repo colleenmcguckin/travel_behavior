484c2d34afd8cf06adeb0e30776ab5a9d1c9ebbc
colleenmcguckin <colleenmcguckin@gmail.com>
2017-01-06 13:11:17 -0800

    center contact modal vertically

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,21 @@
 //= require froala/froala_editor.pkgd.min
 //= require mentions/mentions
 //= require pages
+
+$(function() {
+    function reposition() {
+        var modal = $(this),
+            dialog = modal.find('.modal-dialog');
+        modal.css('display', 'block');
+
+        // Dividing by two centers the modal exactly, but dividing by three
+        // or four works better for larger screens.
+        dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()) / 2));
+    }
+    // Reposition when a modal is shown
+    $('.modal').on('show.bs.modal', reposition);
+    // Reposition when the window is resized
+    $(window).on('resize', function() {
+        $('.modal:visible').each(reposition);
+    });
+});
