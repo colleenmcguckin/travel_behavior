eba89b1c3a8873122c8c8ed981b2dc667d31bb22
colleenmcguckin <colleenmcguckin@gmail.com>
2017-11-12 14:14:13 -0800

    errant comma

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
     domain:               'gmail.com',
     user_name:            ENV["GMAIL_USERNAME"],
     password:             ENV["GMAIL_PASSWORD"],
-    authentication:       :plain,
+    authentication:       :plain
     # enable_starttls_auto: true
   }
 
